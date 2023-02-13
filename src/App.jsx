6326e2550edbc93dac593b41c65ba8ff3a9daa06
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-02-13 09:30:21 -0700

    basic structure commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-// Components
+import React from "react";
+import { Stack } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+
+import CustomHomesAndShops from "./components/CustomHomesAndShops";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import About from "./components/pages/About";
 
 function App() {
-
   return (
-    <div className="App">
-
-    </div>
-  )
+    <>
+      <Stack>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route
+            path="/custom-homes-and-shops"
+            element={<CustomHomesAndShops />}
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Stack>
+      ;
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CustomHomesAndShops.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CustomHomesAndShops() {
+  return <div>CustomHomesAndShops</div>;
+}
