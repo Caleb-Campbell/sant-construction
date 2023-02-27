dd3838fd2409e3f1113e9db281b340ea3cea4524
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-02-27 11:10:31 -0700

    wip

--- src/components/BidModal.jsx
+++ src/components/BidModal.jsx
@@ -1,5 +1,38 @@
+import { Form, FormControl, FormLabel, Modal } from "react-bootstrap";
 import React from "react";
 
-export default function BidModal() {
-  return <></>;
+export default function BidModal({ show, setShow }) {
+  return (
+    <>
+      <Modal show={show}>
+        <Modal.Header>
+          <Modal.Title>Request a Free Bid</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <FormLabel>
+                {" "}
+                Name
+                <FormControl type="text" />
+              </FormLabel>
+              <FormLabel>
+                <FormControl type="text" />
+              </FormLabel>
+              <FormLabel>
+                <FormControl type="email" />
+              </FormLabel>
+              <FormLabel>
+                <FormControl as="select">
+                  <option>Company</option>
+                  <option>Individual</option>
+                  <option>Government</option>
+                </FormControl>
+              </FormLabel>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
