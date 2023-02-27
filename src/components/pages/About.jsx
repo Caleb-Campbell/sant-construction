a86842fd571a7ccef82e15260254c987e8b17c55
Owen McComas <owen@mccomas.me>
2023-02-27 16:49:06 -0700

    About outline complete

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -100,7 +100,38 @@ export default function About() {
           culpa qui officia deserunt mollit anim id est laborum
         </p>
       </div>
-      
+      {/* //////////////////////////////////LEADERSHIP INFO////////////////////////////////// */}
+      <Stack className="bg-light mt-5 mb-5" direction="horizontal">
+        <img src="https://picsum.photos/350/350" className="leaderImg"/>
+        <div className="p-5">
+          <h2>Leadership</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+          <NavLink>Learn more about our leadership {">"}</NavLink>
+        </div>
+      </Stack>
+      <div className="staff-wrap">
+        <div className="staff-row">
+        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+        </div>
+        <div className="staff-row">
+        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+        </div>
+
+      </div>
     </>
   );
 }
