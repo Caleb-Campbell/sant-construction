596a3c525c9379696650f9a9aa325ec0e9e2e1ce
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-03-01 11:05:09 -0700

    fixed merge request

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -8,11 +8,13 @@ export default function About() {
       <Stack className="about-border  mt-5 mb-5" direction="horizontal">
         <img src="https://picsum.photos/500/500" />
         <div className="p-5">
-          <h2><strong>Sant Contruction</strong></h2>
+          <h2>
+            <strong>Sant Contruction</strong>
+          </h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
@@ -104,13 +106,13 @@ export default function About() {
       </div>
       {/* //////////////////////////////////LEADERSHIP INFO////////////////////////////////// */}
       <Stack className="mt-5 mb-5 about-border" direction="horizontal">
-        <img src="https://picsum.photos/350/350" className="leaderImg"/>
+        <img src="https://picsum.photos/350/350" className="leaderImg" />
         <div className="p-5 leadership">
           <h2>Leadership</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
@@ -121,18 +123,49 @@ export default function About() {
       </Stack>
       <div className="staff-wrap">
         <div className="staff-row">
-        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
-        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
-        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
-        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+          <span>
+            <img src="https://picsum.photos/200/200" className="staff-member" />
+            <h3 className="staff-caption-head">Member Name</h3>
+            <p>Member Info</p>
+          </span>
+          <span>
+            <img src="https://picsum.photos/200/200" className="staff-member" />
+            <h3 className="staff-caption-head">Member Name</h3>
+            <p>Member Info</p>
+          </span>
+          <span>
+            <img src="https://picsum.photos/200/200" className="staff-member" />
+            <h3 className="staff-caption-head">Member Name</h3>
+            <p>Member Info</p>
+          </span>
+          <span>
+            <img src="https://picsum.photos/200/200" className="staff-member" />
+            <h3 className="staff-caption-head">Member Name</h3>
+            <p>Member Info</p>
+          </span>
         </div>
         <div className="staff-row">
-        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
-        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
-        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
-        <span><img src='https://picsum.photos/200/200' className="staff-member"/><h3 className="staff-caption-head">Member Name</h3><p>Member Info</p></span>
+          <span>
+            <img src="https://picsum.photos/200/200" className="staff-member" />
+            <h3 className="staff-caption-head">Member Name</h3>
+            <p>Member Info</p>
+          </span>
+          <span>
+            <img src="https://picsum.photos/200/200" className="staff-member" />
+            <h3 className="staff-caption-head">Member Name</h3>
+            <p>Member Info</p>
+          </span>
+          <span>
+            <img src="https://picsum.photos/200/200" className="staff-member" />
+            <h3 className="staff-caption-head">Member Name</h3>
+            <p>Member Info</p>
+          </span>
+          <span>
+            <img src="https://picsum.photos/200/200" className="staff-member" />
+            <h3 className="staff-caption-head">Member Name</h3>
+            <p>Member Info</p>
+          </span>
         </div>
-
       </div>
     </>
   );
