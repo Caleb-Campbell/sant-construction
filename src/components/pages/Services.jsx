5c25b0f7bad1d2b789b99415c8322e9421b0e2d8
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-03-13 18:48:04 -0600

    wip

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import "../../styles/service.css";
+import Service from "../Service";
 
 export default function Services() {
-  return <div className="h-100 p-5 bg-warning">Page under Construction</div>;
+  const services = [
+    "digging",
+    "building",
+    "digging",
+    "building",
+    "digging",
+    "building",
+    "digging",
+    "building",
+    "digging",
+    "building",
+  ];
+
+  return (
+    <div className="service-wrap p-5 bg-dark">
+      <h2 className="text-light service-title">Services</h2>
+      <Service
+        serviceList={services}
+        title={"Stuff"}
+        text={"stuff stuff"}
+        img={
+          "https://images.pexels.com/photos/1579356/pexels-photo-1579356.jpeg"
+        }
+      />
+    </div>
+  );
 }
